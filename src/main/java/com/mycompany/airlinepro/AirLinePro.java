5c77a840fe6598c6e87b1909a21aa9dc14f64978
Princepoulo <Paulompho3@gmail.com>
2025-06-24 14:20:49 +0200

    Initial commit

---
+++ src/main/java/com/mycompany/airlinepro/AirLinePro.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.airlinepro;
+
+/**
+ *
+ * @author Muasawenkosi
+ */
+import java.io.*;
+public class AirLinePro {
+
+    public static void main(String[] args) {
+       
+        //ClientLoginandSgnUp logandSign = new ClientLoginandSgnUp();
+        //logandSign.setVisible(true);
+        WelcomePage welcome = new WelcomePage();
+        welcome.setVisible(true);
+    }
+}
+
+    
+
